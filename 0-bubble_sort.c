5c7dfe5063f8a7015d0b7529fe5d3bad31512050
nedu-ramzi <ramseychinedu@gmail.com>
2023-02-22 00:07:26 +0000

    Write a function that sorts an array of integers in ascending order using the Bubble sort algorithm

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,47 +1,36 @@
 #include "sort.h"
 
 /**
- * bubble_sort - sorts an array of integers using the Bubble sort algorithm
- * @array: pointer to the unsorted array
- * @size: size of the array
- * Return: none(void)
+ * bubble_sort - sorts an array of integers in ascending order using the
+ * Bubble sort algorithm
+ * @array: The array to be sorted
+ * @size: The size of the array
+ *
+ * Return: void
  */
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, j;
-	int swapped;
+	size_t i, n, new_n;
+	int swap;
 
-	if (size <= 2 || array == NULL)
+	if (array == NULL || size < 2)
 		return;
-	for (i = 0; i < size; i++)
+
+	n = size;
+	while (n > 0)
 	{
-		swapped = 0;
-		for (j = 0; j < size - 1; j++)
+		new_n = 0;
+		for (i = 0; i < n - 1; i++)
 		{
-			if (array[j] > array[j + 1])
+			if (array[i] > array[i + 1])
 			{
-				swap(&array[j], &array[j + 1]);
+				swap = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = swap;
+				new_n = i + 1;
 				print_array(array, size);
-				swapped = 1;
 			}
-			else
-				continue;
-		if (swapped == 0)
-			return;
 		}
+		n = new_n;
 	}
-}
-
-/**
- * swap - swaps the position of two integers in an arrat
- * swap - swaps the position of two integers in an arrat
- * @b: second node
- * Return: void
- */
-void swap(int *a, int *b)
-{
-	int temp = *a;
-
-	*a = *b;
-	*b = temp;
 }
